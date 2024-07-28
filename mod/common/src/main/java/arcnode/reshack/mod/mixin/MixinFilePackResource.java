58e96d4e19d294e67fc6c575530c2eecc138cd4c
H3xadecimal <h3xadecimal@afternode.cn>
2024-07-28 13:13:37 +0800

    perf: cleanup

--- mod/common/src/main/java/arcnode/reshack/mod/mixin/MixinFilePackResource.java
+++ mod/common/src/main/java/arcnode/reshack/mod/mixin/MixinFilePackResource.java
@@ -3,13 +3,9 @@ package arcnode.reshack.mod.mixin;
 import arcnode.reshack.mod.ResourceHack;
 import arcnode.reshack.mod.access.ForgeAccessHack;
 import net.minecraft.server.packs.FilePackResources;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.io.File;
 import java.nio.file.Files;
